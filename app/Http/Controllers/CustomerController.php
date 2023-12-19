6bea63d4f4934c1f65fe624bf0e70e8ae35f374f
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-12-19 21:51:14 +0900

    create CustomerController作成

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use App\Http\Requests\CustomerRequest;
+use Illuminate\Support\Facades\Auth;
+
+class CustomerController extends Controller
+{
+ public function index(Request $request) {
+
+    $customers = Customer::paginate(10);
+
+        $company = $request->input('company');
+        $name = $request->input('name');
+        $address = $request->input('address');
+        $email = $request->input('email');
+        $phone = $request->input('phone');
+        $remarks = $request->input('remarks');
+        $user = Auth::user();
+
+        $query = Customer::query();
+
+        if (!empty($company)) {
+            $query = $query->where('company', 'like', '%' . $company . '%');
+        }
+
+        if (!empty($name)) {
+            $query = $query->where('name', 'like', '%' . $name . '%');
+        }
+
+        if (!empty($address)) {
+            $query = $query->where('address', 'like', '%' . $address . '%');
+        }
+
+        if (!empty($email)) {
+            $query = $query->where('email', 'like', '%' . $email . '%');
+        }
+
+        if (!empty($phone)) {
+            $query = $query->orwhere('telephone', 'like', '%' . $phone . '%')
+                           ->orwhere('cellphone', 'like', '%' . $phone . '%');
+        }
+
+        if (!empty($remarks)) {
+            $query = $query->orwhere('company', 'like', '%' . $remarks . '%')
+                            ->orwhere('name', 'like', '%' . $remarks . '%')
+                            ->orwhere('address', 'like', '%' . $remarks . '%')
+                            ->orwhere('cellphone', 'like', '%' . $remarks . '%')
+                            ->orwhere('telephone', 'like', '%' . $remarks . '%')
+                            ->orwhere('remarks', 'like', '%' . $remarks . '%');
+        }
+
+        $customers = $query->paginate(10);
+
+        $allcustomers = [
+            'customers' => $customers,
+            'company' => $company,
+            'name' => $name,
+            'address' => $address,
+            'phone' => $phone,
+            'remarks' => $remarks
+        ];
+
+        return view('customer.index')->with($allcustomers);
+ }
+
+public function showcreate() {
+
+    return view('customer.create');
+
+}
+
+public function executecreate(CustomerRequest $request) {
+
+    $validated = $request->validated();
+
+        $customers = new Customer;
+
+        $customer = $customers->createCustomer($validated);
+
+        if ($customer === false) {
+            return redirect('sales-system/customer/create')->withInput();
+        }
+        return redirect('sales-system/customer');
+
+}
+
+public function edit($id)
+{   
+    $customer = Customer::find($id);
+
+    return view('customer.edit', compact('customer'));
+}
+}
